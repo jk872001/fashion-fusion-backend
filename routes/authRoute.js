3010478f2e34a4d59f10073390017a0b6707cb2f
aashishthakurcodes <j9183208@gmail.com>
2023-02-09 16:02:58 +0530

    jvf

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -6,7 +6,7 @@ const {createUser,loginUser,forgotPassword,resetPassword,getUserById}=require(".
 router.route("/register").post(createUser);
 router.route("/login").post(loginUser);
 router.route("/password/forgot").post(forgotPassword);
-router.route("/password/reset/:token").put(resetPassword);
+router.route("/password/reset/:token").put(resetPassword)
 router.route("/me").get(getUserById);
 
 
